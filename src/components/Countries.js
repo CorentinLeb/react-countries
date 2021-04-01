6e278cff98c854ec2933bb6adde33efb2ef17bfa
Corentin LE BERRE <leberre.corentin@gmail.com>
2021-04-01 09:11:58 +0200

    first commit

---
+++ src/components/Countries.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import Card from "./Card";
+
+const Countries = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+
+        axios
+        .get(
+            "http://restcountries.eu/rest/v2/all?fields=name;poulation;region;capital;flag"
+            )
+        .then((res) => setData(res.data));
+    }, []);
+
+    return (
+        <div className="countries">
+         <ul className="countries-list">
+             {data.map((country) => (
+                <Card country={country} key={country.name} /> 
+             ))}
+         </ul>
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <Logo/>
+            <h1>À Propos</h1>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas expedita quibusdam tenetur voluptate. Doloremque pariatur non obcaecati dolorum, cupiditate, sed illo odio consectetur, omnis eum veniam exercitationem ab perferendis corporis autem labore debitis ex minus fuga dolores deleniti recusandae sit voluptas magnam. Aliquam provident illo doloremque mollitia, quaerat tenetur veniam vel unde architecto natus similique at nihil! Blanditiis, quidem aliquid!
+            </p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas expedita quibusdam tenetur voluptate. Doloremque pariatur non obcaecati dolorum, cupiditate, sed illo odio consectetur, omnis eum veniam exercitationem ab perferendis corporis autem labore debitis ex minus fuga dolores deleniti recusandae sit voluptas magnam. Aliquam provident illo doloremque mollitia, quaerat tenetur veniam vel unde architecto natus similique at nihil! Blanditiis, quidem aliquid!
+            </p>
+        </div>
+    );
+};
+
+export default About;
